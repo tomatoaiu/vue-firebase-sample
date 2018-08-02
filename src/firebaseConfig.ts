b8a7894548d8d87e8b917ff2de22a6d3fb23658d
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-08-02 20:42:54 +0900

    feat: connect firebase

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,26 @@
+import firebase from 'firebase'
+
+declare const process: {
+  env: {
+    NODE_ENV: string,
+    APIKEY: string,
+    AUTHDOMAIN: string,
+    DATABASEURL: string,
+    PROJECTID: string,
+    STORAGEBUCKET: string,
+    MESSAGINGSENDERID: string
+  }
+}
+
+const config = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID
+}
+
+const firebaseApp = firebase.initializeApp(config)
+const db = firebaseApp.database()
+export const dbItemsRef = db.ref('items')
--- src/index.vue
+++ src/index.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
     {{ word }}
+    <button @click="addItems">Add Item</button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { dbItemsRef } from './firebaseConfig'
 
 export default Vue.extend({
   name: 'App',
   data () {
     return {
       word: "Hello, world"
+    }
+  },
+  methods: {
+    addItems (): void {
+      dbItemsRef.push(this.word)
     }
   }
 })
