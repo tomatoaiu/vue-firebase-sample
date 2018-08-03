5182461f1a010d5d0acfaf3d636cabdbe3cc12e8
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-08-03 18:57:21 +0900

    feat: sign in, out  : get set items

--- src/index.vue
+++ src/index.vue
@@ -2,23 +2,55 @@
   <div>
     {{ word }}
     <button @click="addItems">Add Item</button>
+    <button @click="getItems">Get Item</button>
+    <button @click="signIn">Sign in</button>
+    <button @click="signOut">Sign out</button>
+    {{ sign }}
+    <br>
+    {{ user }}
+    <br>
+    {{ items }}
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import firebase from 'firebase'
 import { dbItemsRef } from './firebaseConfig'
 
 export default Vue.extend({
   name: 'App',
   data () {
     return {
-      word: "Hello, world"
+      word: 'Hello, world',
+      sign: 'Sign ?',
+      user: undefined,
+      items: undefined
     }
   },
   methods: {
     addItems (): void {
       dbItemsRef.push(this.word)
+    },
+    getItems (): void {
+      dbItemsRef.once('value')
+      .then((snapshot) => {
+        this.items = snapshot.val()
+      })
+    },
+    signIn (): void {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithPopup(provider).then((result) => {
+        this.user = result.user
+      })
+      this.sign = 'Sign in'
+    },
+    signOut (): void {
+      firebase.auth().signOut().then(() => {
+        this.sign = 'Sign out'
+      }).catch((error) => {
+        // An error happened.
+      });
     }
   }
 })
